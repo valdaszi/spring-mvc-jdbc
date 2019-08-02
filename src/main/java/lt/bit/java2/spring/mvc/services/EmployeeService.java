7512f071bad5c8b723be1e11c20043075052fb5b
Valdas <valdas@e-servisas.lt>
2019-08-02 16:22:45 +0300

    ismesta nereikalinga @Repository anotacija

--- src/main/java/lt/bit/java2/spring/mvc/services/EmployeeService.java
+++ src/main/java/lt/bit/java2/spring/mvc/services/EmployeeService.java
@@ -3,11 +3,9 @@ package lt.bit.java2.spring.mvc.services;
 import lt.bit.java2.spring.mvc.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EmployeeService extends JpaRepository<Employee, Integer> {
 
 //    @Query("SELECT .... ")
